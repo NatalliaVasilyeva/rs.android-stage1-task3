ee263597e3db8fd7a88576239cbb5d5239932004
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-05-04 23:07:55 +0300

    feat: add subtask "Decomposer"

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,39 @@
 package subtask4
 
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decomposeRecursia(number * number, number - 1);
+
+    }
+
+    private fun decomposeRecursia(rest: Int, number: Int): Array<Int>? {
+
+        for (i in number downTo 1) {
+            var square = i * i;
+            var nRest = rest - square;
+            if (nRest == 0) {
+                return arrayOf(i);
+            }
+            if (nRest < 0) {
+                return null;
+            }
+
+            var squareRoot = floor(sqrt(nRest.toDouble())).toInt();
+
+            if (squareRoot >= i) {
+                squareRoot = i - 1
+            }
+
+            var result = decomposeRecursia(nRest, squareRoot);
+
+            if (result != null) return result + arrayOf(i)
+
+        }
+        return null;
+
     }
 }
