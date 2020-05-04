9617413f1c0be45f137f26bbf717f17c400a11c4
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-05-04 20:32:46 +0300

    feat: add subtask "ArrayCalculator"

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,47 @@
 package subtask3
 
+import kotlin.math.absoluteValue
+
+
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var multiplication = 1;
+        var sortedArray =
+            itemsFromArray.filterIsInstance<Int>().sortedByDescending { it.absoluteValue };
+        val positiveIntArray = sortedArray.filter { i -> i > 0 }
+        var negativeIntArray = sortedArray.filter { i -> i < 0 }
+        var allNumbersMultiplication = 1
+        var positiveNumberMultiplication = 1;
+
+
+        if (sortedArray.size <= numberOfItems) {
+            if (sortedArray.isEmpty()) {
+                return 0;
+            } else {
+                sortedArray.indices.forEach { i ->
+                    multiplication *= sortedArray[i];
+                }
+            }
+            return multiplication;
+        }
+
+        if (negativeIntArray.size <= 1) {
+            for (i in 0 until numberOfItems) {
+                multiplication *= positiveIntArray[i];
+            }
+            return multiplication;
+        }
+
+        for (i in 0 until numberOfItems) {
+            allNumbersMultiplication *= sortedArray[i];
+            positiveNumberMultiplication *= positiveIntArray[i];
+        }
+
+        return if (allNumbersMultiplication >= positiveNumberMultiplication) {
+            allNumbersMultiplication;
+        } else {
+            positiveNumberMultiplication;
+        };
     }
 }
